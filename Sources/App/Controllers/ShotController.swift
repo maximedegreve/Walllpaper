14b03a5414e669f6bf5edc230ef11007f1dee825
Maxime De Greve <Maxime@Maximes-MacBook-Pro-2.local>
2017-02-26 13:01:27 +0000

    Consented

--- Sources/App/Controllers/ShotController.swift
+++ Sources/App/Controllers/ShotController.swift
@@ -2,42 +2,25 @@ import Vapor
 import HTTP
 
 final class ShotController: ResourceRepresentable {
+    
     func index(request: Request) throws -> ResponseRepresentable {
-        return try Shot.all().makeJSON()
-    }
+        
+        guard let categoryString = request.data["category"]?.string else {
+            throw Abort.custom(status: .badRequest, message: "No category was defined")
+        }
 
-    func show(request: Request, post: Shot) throws -> ResponseRepresentable {
-        return post
-    }
+        guard let category = try Category.query().filter("name", categoryString).first() else {
+            throw Abort.custom(status: .badRequest, message: "Category not found")
+        }
+        
+        return try category.shots().union(User.self).filter(User.self, "consented", true).all().makeJSON()
 
-    func delete(request: Request, post: Shot) throws -> ResponseRepresentable {
-        try post.delete()
-        return JSON([:])
+        
     }
-
-    func clear(request: Request) throws -> ResponseRepresentable {
-        try Shot.query().delete()
-        return JSON([])
-    }
-
-    /*
-    func update(request: Request, post: Shot) throws -> ResponseRepresentable {
-        let new = try request.shot()
-        var post = post
-        post.content = new.content
-        try post.save()
-        return post
-    }
- */
-
 
     func makeResource() -> Resource<Shot> {
         return Resource(
-            index: index,
-            show: show,
-            //modify: update,
-            destroy: delete,
-            clear: clear
+            index: index
         )
     }
 }
